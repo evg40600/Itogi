0406ce8944b81b6230dea5752bccbe195a74e396
evg4060046 <4060046@gmail.com>
2023-02-04 15:50:40 +0500

    Пройдем массив и найдём кол-во подходящих

--- Itogi/Program.cs
+++ Itogi/Program.cs
@@ -5,6 +5,13 @@ Console.Clear();
 
 String[] base_array = {"hello","2","world",":-)"};
 
+int k = 0;
+
+for (int i = 0; i < base_array.Length; i++)
+{
+    if (base_array[i].Length <= 3)
+        k = k + 1;
+
+}    
 
 Console.WriteLine(k);
-
